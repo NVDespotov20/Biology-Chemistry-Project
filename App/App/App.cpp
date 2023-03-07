b3e50d1fb83aca7f0a520de80fe05bffd191aeae
Denislav Bratoevski <dgbratoevski20@codingburgas.bg>
2023-03-07 18:58:14 +0200

    Add human body and buttons to turn it sideways

--- App/App/App.cpp
+++ App/App/App.cpp
@@ -1,9 +1,31 @@
 #include"App.hpp"
+#include <iostream>
+using namespace std;
+
 App::App() {
 
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 450, "Human muscles");
 
     SetTargetFPS(60);
+
+    previousButton = LoadTexture("../src/images/previous.png");
+    nextButton = LoadTexture("../src/images/next.png");
+
+    previousButton.width = 50;
+    previousButton.height = 75;
+
+    nextButton.width = 50;
+    nextButton.height = 75;
+
+    nextButtonRec = {735, 200, float(nextButton.width) ,float(nextButton.height)};
+    previousButtonRec = { 10, 200, float(previousButton.width) ,float(previousButton.height)};
+
+    humanBody = LoadTexture("../src/images/Human_body.png");
+
+    sideOfHumanFloat = 170;
+
+    sideOfHumanRec = { 0, 0, sideOfHumanFloat-10, 250 };
+    sideOfHumanVec = {300, 100};
 }
 App::~App() {
     delete this;
@@ -13,11 +35,25 @@ void App::loop()
 {
     while (!WindowShouldClose())
     {
+        mousePoint = GetMousePosition();
+
+        ClearBackground(LIGHTGRAY);
+
+        DrawTextureRec(humanBody, sideOfHumanRec, sideOfHumanVec, WHITE);
+
+        DrawTexture(previousButton,10,200,WHITE);
+        DrawTexture(nextButton, 735, 200, WHITE);
 
 
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, nextButtonRec))
+        {
+            sideOfHumanRec.x = sideOfHumanRec.x + 160;
+        }
+        
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, previousButtonRec))
+        {
+            sideOfHumanRec.x = sideOfHumanRec.x - 160;
+        }
 
         EndDrawing();
     }
--- App/App/App.hpp
+++ App/App/App.hpp
@@ -3,6 +3,20 @@
 class App
 {
 public:
+	Vector2 mousePoint;
+
+	Texture2D humanBody;
+
+	Texture2D nextButton;
+	Texture2D previousButton;
+
+	Rectangle nextButtonRec;
+	Rectangle previousButtonRec;
+
+	Rectangle sideOfHumanRec;
+	Vector2 sideOfHumanVec;
+	float sideOfHumanFloat;
+
 	App();
 	~App();
 	void loop();
